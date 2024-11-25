36f6f9481bd379b9e4ee97a7dc44e2843b1820be
Kevin <kuanhongzheng84@gmail.com>
2024-11-25 14:10:59 +0800

    turn salesNumberDto into record

--- hestia/src/main/java/com/ispan/hestia/dto/SalesNumbersDTO.java
+++ hestia/src/main/java/com/ispan/hestia/dto/SalesNumbersDTO.java
@@ -1,22 +1,4 @@
 package com.ispan.hestia.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class SalesNumbersDTO {
-
-	private String month;
-
-	private Integer totalSales;
-
-	private Long orderCount;
-
-	SalesNumbersDTO(String month, Integer totalSales, Long orderCount) {
-		this.month = month;
-		this.totalSales = totalSales;
-		this.orderCount = orderCount;
-	}
-
+public record SalesNumbersDTO(String month, Integer totalSales, Long orderCount) {
 }
